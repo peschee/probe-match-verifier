f16fd4d4c228c2c016741befaad1b8dc9f46d7f8
Peter Siska <psiska@inventage.com>
2021-12-29 22:39:41 +0100

    Automatically import meta.scss file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-  // build: {
-  //   rollupOptions: {
-  //     external: /^lit/,
-  //   },
-  // },
+  resolve: {
+    alias: {
+      '~@': path.resolve(__dirname, '/src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "~@/meta.scss";`,
+      },
+    },
+  },
 });
