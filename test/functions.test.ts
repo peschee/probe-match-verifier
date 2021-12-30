e6c52903d6912a83f71cf9202c8cf19870815959
Peter Siska <psiska@inventage.com>
2021-12-30 10:57:00 +0100

    Add format number tests

--- test/functions.test.ts
+++ test/functions.test.ts
@@ -1,8 +1,18 @@
 import { assert, test } from 'vitest';
-import { relativeDifference } from '../src/util/functions';
+
+import { relativeDifference, formatNumber } from '../src/util/functions';
 
 test('relativeDifference()', async () => {
   assert.equal(relativeDifference(10, 5), -100);
   assert.equal(relativeDifference(5, 10), 50);
   assert.equal(relativeDifference(2, 10), 80);
+});
+
+test('formatNumber()', async () => {
+  assert.equal(formatNumber(10), '10.00000');
+  assert.equal(formatNumber(10.00001), '10.00001');
+  assert.equal(formatNumber(10.000012), '10.00001');
+  assert.equal(formatNumber(10.000015), '10.00001');
+  assert.equal(formatNumber(10.0000156), '10.00002');
+  assert.equal(formatNumber(10.000016), '10.00002');
 });
