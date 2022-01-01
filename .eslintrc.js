e1d007ca4eecec7373651f4f6a41740dd44953ba
Peter Siska <63866+peschee@users.noreply.github.com>
2022-01-01 19:25:09 +0100

    Add eslint plugins for lit + wc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:lit/recommended', 'plugin:wc/recommended'],
   overrides: [
     {
       files: '.eslintrc.js',
@@ -11,4 +11,9 @@ module.exports = {
       },
     },
   ],
+  settings: {
+    wc: {
+      elementBaseClasses: ['LitElement'],
+    },
+  },
 };
