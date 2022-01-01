6b27b5996077d12053916c549aa1f3e942d43eba
Peter Siska <63866+peschee@users.noreply.github.com>
2022-01-01 19:27:16 +0100

    Lit plugin order change

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:lit/recommended', 'plugin:wc/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:wc/recommended', 'plugin:lit/recommended'],
   overrides: [
     {
       files: '.eslintrc.js',
